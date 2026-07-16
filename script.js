afe15c0ba7729dece7a1da5dc644553ad69d78ac
ibrhmtrq <i.mk.tariq@gmail.com>
2026-07-17 00:49:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var filters = [];
 function filteradd(filterino) {
-  filters.push(filterino);
+  //filters.push(filterino);
   console.log(filterino);
-  console.log(filterino);
+  console.log(filteradd);
 }
