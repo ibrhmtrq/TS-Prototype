0558836e24911a4a532bff6c010d6e6029581660
ibrhmtrq <i.mk.tariq@gmail.com>
2026-07-17 01:51:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var filters = [];
 function filteradd(filterino) {
   if (!filters.includes(filterino)) {
       filters.push(filterino);
+      window.location.href = 'storefront.html';
   }
   else if (filters.includes(filterino)) {
     filters.splice(filters.indexOf(filterino), 1);
