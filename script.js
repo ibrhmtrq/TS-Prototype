75112b961d9ccc6274e8f42f01a44a39f1faeaa9
ibrhmtrq <i.mk.tariq@gmail.com>
2026-07-21 02:46:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function filteradd(filterino) {
       window.location.href = 'storefront.html';
   }
   else if (filters.includes(filterino)) {
+    sessionStorage.setItem("filters", JSON.stringify(filters));
     filters.splice(filters.indexOf(filterino), 1);
   }
   console.log(filterino);
@@ -18,10 +19,11 @@ function qualifyItems(item) {
   for (var i = 0; i < merchList.length; i++) {
     merchList[i].style.display = "block";
     for (var j = 0; j < filters.length; j++) {
-      if (merchList[i].List.indexOf(filters[j]) == -1)
-      {
-        merchList[i].style.display = "none";
-      }
+      // if (merchList[i].List.indexOf(filters[j]) == -1)
+      // {
+      //   merchList[i].style.display = "none";
+      // }
+      console.log(merchList[i];
     } 
   }
 }
