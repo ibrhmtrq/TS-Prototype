d5c0d458d21e1960dd69326d5a74ac3a6b0e89fc
ibrhmtrq <i.mk.tariq@gmail.com>
2026-07-23 11:28:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function navHome() {
 //   console.log(filters);
 // }
 function filteradd(filterino) {
-  const filters = JSON.parse(sessionStorage.getItem("filters")) || [];
+  let filters = JSON.parse(sessionStorage.getItem("filters")) || [];
   filters = (filterino);
   sessionStorage.setItem("filters", JSON.stringify(filters));
   window.location.href = 'storefront.html';
@@ -30,7 +30,7 @@ function filteradd(filterino) {
 }
 
 function qualifyItems() {
-  const filters = JSON.parse(sessionStorage.getItem("filters")) || [];
+  let filters = JSON.parse(sessionStorage.getItem("filters")) || [];
   for (var i = 0; i < merchList.length; i++) {
     merchList[i].style.display = "block";
     for (var j = 0; j < filters.length; j++) {
